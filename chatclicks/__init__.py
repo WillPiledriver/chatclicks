54efe8f4f6484497fc6c5871549fc4c8f6e0a9c1
WillPiledriver <willpiledriver@gmail.com>
2023-08-10 17:34:13 -0400

    init

---
+++ chatclicks/__init__.py
@@ -0,0 +1,60 @@
+import socketio
+
+class ChatClicks():
+    def __init__(self, channel_id):
+        self.channel_id = channel_id
+        self.sio = socketio.AsyncClient()
+
+        self.event_handlers = {}
+        # Register events with decorated methods
+        self.sio.on("connect", handler=self.connect)
+        self.sio.on("message", handler=self.message)
+        self.sio.on("leftClick", handler=self.leftClick)
+        self.sio.on("rightClick", handler=self.rightClick)
+        self.sio.on("init", handler=self.init)
+        self.sio.on("connect_error", handler=self.connect_error)
+        self.sio.on("disconnect", handler=self.disconnect)
+
+    def event(self, name):
+        # Decorator function that takes the event name
+        def decorator(func):
+            # Inner decorator to wrap the event handler function
+            async def async_wrapper(data):
+                await func(data)  # Ensure you await the async function inside the event handler
+
+            self.event_handlers[name] = async_wrapper  # Store the event handler
+            return async_wrapper
+        return decorator
+
+    async def connect(self):
+        print("Websocket connected.")
+        await self.sio.emit("init", self.channel_id)
+
+    async def message(self, data):
+        print('Received message:', data)
+
+    async def leftClick(self, data):
+        if "leftClick" in self.event_handlers:
+            self.event_handlers["leftClick"](data)
+
+    async def rightClick(self, data):
+        if "rightClick" in self.event_handlers:
+            self.event_handlers["rightClick"](data)
+
+    async def init(self, data):
+        print(self.event_handlers.keys())
+        if "init" in self.event_handlers:
+            await self.event_handlers["init"](data)
+        else:
+            if data == self.channel_id:
+                print("Echo received: " + self.channel_id)
+
+    async def connect_error(self, data):
+        print("The websocket connection failed!")
+
+    async def disconnect(self):
+        print("Disconnected from websocket.")
+
+    async def run(self):
+        await self.sio.connect("https://willpile.com:8080")
+        await self.sio.wait()
