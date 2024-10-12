5feb4059ab1bb939cc4417c51e85e19e47aeacbf
WillPiledriver <willpiledriver@gmail.com>
2024-10-12 05:49:30 -0400

    v4.2

--- chatclicks/__init__.py
+++ chatclicks/__init__.py
@@ -1,22 +1,28 @@
-import socketio
 import asyncio
 import time
+import socketio
 import numpy as np
 from sklearn.cluster import DBSCAN
 
 class ChatClicks():
-    def __init__(self, channel_id, sub_only=False, allow_anonymous=True, ban_list=[], max_poll_time=10, sub_boost=1, priority_boost=4, priority_votes=5, tug_weight=10, dimensions="1920x1080", check_coords_func=None, poll_callback=None):
+    """
+    Bot for the ChatClicks Twitch Extension
+    """
+    def __init__(self, channel_id, sub_only=False, allow_anonymous=True, ban_list=None, max_poll_time=10, sub_boost=1, priority_boost=4, priority_votes=5, tug_weight=10, dimensions="1920x1080", check_coords_func=None, poll_callback=None):
         self.channel_id = channel_id
         self.allow_anonymous = allow_anonymous
         self.sub_only = sub_only
-        self.ban_list = set([name.lower() for name in ban_list])
+        if ban_list is None:
+            self.ban_list = set()
+        else:
+            self.ban_list = set(name.lower() for name in ban_list)
         self.sub_boost = sub_boost
         self.priority_boost = priority_boost
         self.priority_votes = priority_votes
         self.sio = socketio.AsyncClient()
         self.max_poll_time = max_poll_time
-        self.poll_dict = dict()
-        self.priority_dict = dict()
+        self.poll_dict = {}
+        self.priority_dict = {}
         self.loop = None
         self.clock_offset = 0
         self.check_coords_func = check_coords_func
@@ -36,18 +42,28 @@ class ChatClicks():
         self.sio.on("init", handler=self.init)
         self.sio.on("connect_error", handler=self.connect_error)
         self.sio.on("disconnect", handler=self.disconnect)
-    
+
     @property
     def tug_of_war(self):
+        """
+        Returns the current tug of war value
+        """
         return self._tug_of_war
 
     @tug_of_war.setter
     def tug_of_war(self, value):
+        """
+        Sets the tug of war value and clamps it to be between -100 and 100.
+        :param value: The new tug of war value.
+        """
         # Clamp the value to be between -100 and 100
         self._tug_of_war = max(-100, min(100, value))
     
     @property
     def poll_time(self):
+        """
+        Returns the poll time based on the tug of war value
+        """
         if self.tug_of_war < 1:
             return 0.05
         return max(0, (self._tug_of_war / 100) * self.max_poll_time - 1) + 1
@@ -64,10 +80,14 @@ class ChatClicks():
         return decorator
 
     async def connect(self) -> None:
+        """
+        Sends an init packet to the server on connection
+        """
         print("Websocket connected.")
         await self.sio.emit("init", self.channel_id)
-    
+
     async def message(self, data: dict) -> None:
+        """Received a message from the server"""
         print('Received message:', data)
 
     async def add_data(self, data: dict, click: str) -> None:
@@ -77,10 +97,9 @@ class ChatClicks():
         :param click: This is the user's click.
         """
 
-
         if data["opaque_id"] in self.poll_dict:
             return
-        
+
         if click != "drag":
             if not 0 <= data["x"] < 1:
                 return
@@ -95,18 +114,18 @@ class ChatClicks():
                 return
             if not 0 <= data["end"]["y"] < 1:
                 return
-        
-        
+
+
         data["type"] = {"leftClick": "left", "rightClick": "right", "drag": "drag"}[data["action"]]
 
         if data["type"] != "drag":
-            data["x"] = round(data["x"] * self.dimensions[0])
-            data["y"] = round(data["y"] * self.dimensions[1])
+            data["x"] = data["x"] * self.dimensions[0]
+            data["y"] = data["y"] * self.dimensions[1]
         else:
-            data["start"]["x"] = round(data["start"]["x"] * self.dimensions[0])
-            data["start"]["y"] = round(data["start"]["y"] * self.dimensions[1])
-            data["end"]["x"] = round(data["end"]["x"] * self.dimensions[0])
-            data["end"]["y"] = round(data["end"]["y"] * self.dimensions[1])
+            data["start"]["x"] = data["start"]["x"] * self.dimensions[0]
+            data["start"]["y"] = data["start"]["y"] * self.dimensions[1]
+            data["end"]["x"] = data["end"]["x"] * self.dimensions[0]
+            data["end"]["y"] = data["end"]["y"] * self.dimensions[1]
 
         if self.check_coords_func is not None:
             if not await self.check_coords_func(data):
@@ -118,7 +137,7 @@ class ChatClicks():
             self.priority_dict[data["opaque_id"]]["n"] -= 1
             if self.priority_dict[data["opaque_id"]]["n"] == 0:
                 del self.priority_dict[data["opaque_id"]]
-        
+
         if data["subscribed"]:
             n += self.sub_boost
 
@@ -184,7 +203,7 @@ class ChatClicks():
 
         if "rightClick" in self.event_handlers:
             await self.event_handlers["rightClick"](data)
-    
+
     async def drag(self, data: dict) -> None:
         """
         Called when a user drags on the screen.
@@ -197,14 +216,14 @@ class ChatClicks():
         if not self.allow_anonymous:
             if data["opaque_id"].startswith("A"):
                 return
-            
+
         data["event_time"] -= self.clock_offset
 
         await self.add_data(data, "drag")
 
         if "drag" in self.event_handlers:
             await self.event_handlers["drag"](data)
-    
+
     async def bits(self, data: dict) -> None:
         """
         Called when a user uses bits.
@@ -235,6 +254,10 @@ class ChatClicks():
                 print("Bits Data:", data)
 
     async def init(self, data: dict) -> None:
+        """
+        Called when the server sends an init event.
+        :param data: This is the user's data sent from the server.
+        """
         if data["id"] == self.channel_id:
             print("Echo received: " + self.channel_id)
             self.clock_offset = time.time() - data["time"]
@@ -243,19 +266,39 @@ class ChatClicks():
                 await self.event_handlers["init"](data)
 
     async def connect_error(self, data) -> None:
+        """
+        Called when the websocket connection fails.
+        :param data: This is the error data.
+        """
         print("The websocket connection failed!", data)
 
     async def disconnect(self) -> None:
+        """
+        Called when the websocket connection is closed.
+        """
         print("Disconnected from websocket.")
-    
+
     async def ban_username(self, name: str) -> None:
+        """
+        Bans a specific username from the bot.
+        :param name: The username to ban.
+        """
         self.ban_list.add(name.lower())
-    
+
     async def unban_username(self, name: str) -> None:
+        """
+        Unbans a specific username from the bot.
+        :param name: The username to unban.
+        """
         if name.lower() in self.ban_list:
             self.ban_list.remove(name.lower())
 
     async def find_center_cluster(self, poll_dict: dict) -> dict:
+        """
+        Finds the center of the click clusters in the poll_dict.
+        :param poll_dict: The dictionary containing user data.
+        :return: The center coordinates of the click clusters.
+        """
         # Extract coordinates and click information from the click_dict
         x_coordinates = []
         y_coordinates = []
@@ -277,10 +320,10 @@ class ChatClicks():
                 y_start = coords["start"]["y"]
                 x_end = coords["end"]["x"]
                 y_end = coords["end"]["y"]
-                
+
                 drag_start_coordinates.extend([(x_start, y_start)] * n)
                 drag_end_coordinates.extend([(x_end, y_end)] * n)
-                
+
                 click_counts["drag"] += n
             else:
                 # For regular clicks
@@ -310,19 +353,19 @@ class ChatClicks():
             center = np.mean(most_populated_cluster, axis=0)
 
 
-            x, y = (round(center[0]), round(center[1]))
+            x, y = (center[0], center[1])
 
         # Now, calculate centers for drag start and end coordinates
         if len(drag_start_coordinates) > 0:
             drag_start_coordinates = np.array(drag_start_coordinates)
             center_start = np.mean(drag_start_coordinates, axis=0)
-            x_start, y_start = (round(center_start[0]), round(center_start[1]))
+            x_start, y_start = (center_start[0], center_start[1])
 
         if len(drag_end_coordinates) > 0:
             drag_end_coordinates = np.array(drag_end_coordinates)
             center_end = np.mean(drag_end_coordinates, axis=0)
-            x_end, y_end = (round(center_end[0]), round(center_end[1]))
-        
+            x_end, y_end = (center_end[0], center_end[1])
+
         if most_clicked_key.startswith("drag"):
             return {"type": "drag", "start": {"x": x_start, "y": y_start}, "end": {"x": x_end, "y": y_end}}
         elif most_clicked_key in ["left", "right"]:
@@ -331,6 +374,9 @@ class ChatClicks():
             return None
 
     async def click_loop(self):
+        """
+        This is the main loop for polling the clicks.
+        """
         while True:
             try:
                 await asyncio.sleep(self.poll_time)
@@ -344,13 +390,19 @@ class ChatClicks():
             except Exception as e:
                 self.poll_dict = dict()
                 print(e)
-    
+
     async def start(self):
+        """
+        Starts the websocket connection and the click polling loop.
+        """
         self.loop.create_task(self.click_loop())
         await self.sio.connect("https://willpile.com:8080")
         await self.sio.wait()
 
     def run(self):
+        """
+        Runs the bot.
+        """
         self.loop = asyncio.get_event_loop()
         return self.loop.create_task(self.start())
         
